3ceb33103bb3e2280d90e9038229c939309c3235
LuriDDD <iamyourasistan@gmail.com>
2023-09-12 01:19:50 +1000

    Fix VectorBuf

--- VectorBuf.h
+++ VectorBuf.h
@@ -24,18 +24,22 @@ protected:
     T* buf_;
     size_t sz_, used_ = 0;
 
-    VectorBuf() : buf_{nullptr}, sz_{0} {};
-    explicit VectorBuf(size_t sz) : buf_{::operator new(sz_ * sizeof(T))}, sz_{sz} {};
+    explicit VectorBuf(size_t sz = 0) : buf_{(sz == 0) ?
+                                                nullptr :
+                                                static_cast<T*>(::operator new(sz * sizeof(T)))}, sz_{sz} {};
 
     VectorBuf(const VectorBuf& rhs) = delete;
     VectorBuf& operator=(const VectorBuf& rhs) = delete;
 
-    VectorBuf(VectorBuf&& rhs) : buf_{rhs.buf_} {
+    VectorBuf(VectorBuf&& rhs) noexcept : buf_{rhs.buf_}, sz_{rhs.sz_}, used_{rhs.used_} {
+        rhs.buf_ = nullptr;
+        rhs.sz_ = rhs.used_ = 0;
+    };
+    VectorBuf& operator=(VectorBuf&& rhs) noexcept {
+        std::swap(buf_, rhs.buf_);
         std::swap(sz_, rhs.sz_);
         std::swap(used_, rhs.used_);
-    };
-    VectorBuf& operator=(VectorBuf&& rhs) {
-        std::swap(*this, rhs);
+        return *this;
     };
     ~VectorBuf() {
         destroy(buf_, buf_ + used_);
--- main.cpp
+++ main.cpp
@@ -2,7 +2,14 @@
 
 #include "VectorBuf.h"
 
-
+struct X {
+    X() { std::cout << "X()" << std::endl; }
+    X(const X&) { std::cout << "X(&)" << std::endl; }
+    X& operator=(const X&) { std::cout << "=X(&)" << std::endl; return *this; }
+    X(X&&) { std::cout << "X(&&)" << std::endl; }
+    X& operator=(X&&) { std::cout << "=X(&&)" << std::endl; return *this;}
+    ~X() { std::cout << "~X()" << std::endl; }
+};
  
 int main()
 {
